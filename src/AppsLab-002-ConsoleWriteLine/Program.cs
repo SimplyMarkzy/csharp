84c344656ee8d5278adad2a180dde9640ffc618a
SimplyMarkzy <13markuso@gmail.com>
2025-09-10 08:53:23 +0200

    change of console write line txt from hello world to hello markus

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+//use for comments 
+Console.WriteLine("Hello, Markus!");
+Console.WriteLine("Hello, Markus!");
+//Console.WriteLine("Hello, Markus!");
+Console.WriteLine("Hello, Markus!");
+/*makes the whole segment until the ending 7
+ a comment just like so
+*/
+

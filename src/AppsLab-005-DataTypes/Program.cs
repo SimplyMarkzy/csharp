846bae4848866a47d960adee3c1f3d92d6932b7a
SimplyMarkzy <13markuso@gmail.com>
2025-09-10 10:04:08 +0200

    console.write lines ex bool,int,float,string,char

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdult = false;
+            Console.WriteLine(isAdult);
+            
+            Console.WriteLine("Hello Markzy");
+            int MyAge = 15;
+            Console.WriteLine(MyAge);
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+            string name = "Markzy";
+            Console.WriteLine(name);
+            char gender = 'P';
+            Console.WriteLine(gender);
+
+
+
+
+
+
+
+        }
+    }
+}
+

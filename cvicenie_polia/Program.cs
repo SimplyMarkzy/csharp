857f1032a77bbf20e90b0750a20961830e3418d5
smehylm25 <smehylm25@Z00T1-06>
2025-10-01 12:52:13 +0200

    cycles numero 2

--- cvicenie_polia/Program.cs
+++ cvicenie_polia/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace cvicenie_polia
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace cvicenie_polia
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   
+        {
             /*for (int i = 0; i < numbers.Length; i++)
             int[] numbers = new int[6];
             numbers[1] = 15;
@@ -24,20 +27,31 @@
              Console.WriteLine(sum);
             */
             //cez jeden cyklus naplnit pole hodnotami pomocou write line a nasledne sctitat vsetky hodnoty #
-            int[] number = new int[6];
-            int sum = 0;
-            for (int i = 0; i < 6; i++)
-            {
-                Console.WriteLine("zadaj cisla");
-                number[i] = int.Parse(Console.ReadLine());
-                sum += number[i];
-                
+
             
+            int sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("how many numbers do you want");
+                int number1 = int.Parse(Console.ReadLine());
+                int[] number = new int[number1];
+                for (int i = 0; i < number1; i++)
+                {
+                    Console.WriteLine("zadaj cisla");
+                    number[i] = int.Parse(Console.ReadLine());
+                }
+                foreach (int i in number)
+                {
+                    sum += i;
+                }
+
+
+                Console.WriteLine(sum);
+                string input = Console.ReadLine();
+                if (input == "exit")
+                break;
             }
-               Console.WriteLine(sum);
-
-
-
 
 
 

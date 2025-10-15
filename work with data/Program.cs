8ee24e1076531cc3b091459c3f47b4dce45f8f57
Markzy <13markuso@gmail.com>
2025-10-15 12:10:37 +0200

    work with data

---
+++ work with data/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace work_with_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // we are gonna work with data in this file :)
+            string[] text = File.ReadAllLines("People_100.csv");
+            MoneyCountAverage(text);
+            AllSocialSecurityNumbers(text);
+            NameWithLeastMoney(text);
+        }
+        public static void MoneyCountAverage(string[] text)
+        {
+            int sum = 0;
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(';');
+                int value = int.Parse(splits[4]);
+                sum += value;
+            }
+            Console.WriteLine(sum / (text.Length - 1));
+
+        }
+        public static void AllSocialSecurityNumbers(string[] text)
+        {
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(';');
+                string social = splits[2];
+                Console.WriteLine(social);
+            }
+        }
+        public static void NameWithLeastMoney(string[] text)
+        {
+            string name = "";
+            int min = int.MaxValue;
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(';');
+                int value = int.Parse(splits[4]);
+                if (value < min)
+                {
+                    min = value;
+                    name = splits[0]  +  splits[1];
+                    
+                }
+            }
+            Console.WriteLine(name);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ work with strings and math/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata;
+
+namespace work_with_strings_and_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int result = Add(a, b ,5, 10, 15);
+            Console.WriteLine(result);
+        }
+        public static int Add(int a, int b, int c, int d, int e)
+        {
+          
+           
+
+
+            int allnumb = a + b + c + d + e;
+            return allnumb;
+
+        }
+    }
+}
